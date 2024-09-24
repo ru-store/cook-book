f86b3adff1d29f9174f03fbf9378d455a606d923
ozero reka <rahmalva@icloud.com>
2024-09-24 16:17:56 +0300

    compot

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-with open("cook.txt") as file:
+with (open("cook.txt") as file):
     cook_book = {}
+    for dish in file:
+        name_dish = dish.strip()
+        number_ingredients = int(file.readline())
+        ingredients = []
+        for number in range(number_ingredients):
+            ingredient = file.readline().strip()
+            ingredient_name, quantity, measure = ingredient.split(" | ")
+            ingredients.append({"ingredient": ingredient_name, "quantity": quantity, "measure": measure})
+            cook_book[name_dish] = ingredients
+            file.readline()
+
+
