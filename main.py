cd582f910a5671613f119466e462baf6a4588d33
ozero reka <rahmalva@icloud.com>
2024-09-27 10:31:03 +0300

    work1end

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-with (open("cook.txt") as file):
+with open("cook.txt") as file:
     cook_book = {}
-    for dish in file:
-        name_dish = dish.strip()
+    for line in file:
+        name_dish = line.strip()
         number_ingredients = int(file.readline())
         ingredients = []
         for number in range(number_ingredients):
             ingredient = file.readline().strip()
-            ingredient_name, quantity, measure = ingredient.split(" | ")
-            ingredients.append({"ingredient": ingredient_name, "quantity": quantity, "measure": measure})
-            cook_book[name_dish] = ingredients
-            file.readline()
+            ingredient_name, quantity, measure = ingredient.split(' | ')
+            ingredients.append({'ingredient': ingredient_name, 'quantity': quantity, 'measure': measure})
+        file.readline()
+        cook_book[name_dish] = ingredients
+    print(cook_book)
 
 
