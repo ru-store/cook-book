43ce7b1aef013f567595ccf52241b567ff6cd756
ozero reka <rahmalva@icloud.com>
2024-09-28 20:03:40 +0300

    work2

--- main.py
+++ main.py
@@ -8,8 +8,23 @@ with open("cook.txt") as file:
             ingredient = file.readline().strip()
             ingredient_name, quantity, measure = ingredient.split(' | ')
             ingredients.append({'ingredient': ingredient_name, 'quantity': quantity, 'measure': measure})
+
+
         file.readline()
         cook_book[name_dish] = ingredients
     print(cook_book)
+
+def get_shop_list_by_dishes(dishes, person_count):
+    list_products = {}
+    for dish in dishes:
+        if dish in cook_book:
+            for eating in cook_book[dish]:
+                list_products.values({cook_book[measure]}, {int(cook_book[quantity])} * int(person_count))
+        else:
+            print(f'Блюда "{dish}" нет в книге рецептов')
+    return list_products
+
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+
 
 
---
+++ test.py
@@ -0,0 +1,5 @@
+with open("cook.txt") as file:
+    for line in file:
+
+
+        print(file.readline().strip())
