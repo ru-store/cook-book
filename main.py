60e748892399a0d8666c2115af097d4360a1246a
ozero reka <rahmalva@icloud.com>
2024-10-02 12:58:51 +0300

    overwork1-2

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ with open("cook.txt") as file:
     cook_book = {}
     for line in file:
         name_dish = line.strip()
-        number_ingredients = int(file.readline())
+        quantity_ingredients_in_dish = int(file.readline())
         ingredients = []
-        for number in range(number_ingredients):
+        for counter in range(quantity_ingredients_in_dish):
             ingredient = file.readline().strip()
             ingredient_name, quantity, measure = ingredient.split(' | ')
             ingredients.append({'ingredient': ingredient_name, 'quantity': quantity, 'measure': measure})
@@ -15,19 +15,24 @@ with open("cook.txt") as file:
     print(cook_book)
 
 def get_shop_list_by_dishes(dishes, person_count):
-    list_products = []
+    list_products = {}
     for dish in dishes:
-        if dish in cook_book[dish]:
-            for eating in dish:
-                list_products.append({"ingredient": cook_book[dish].get(ingredient_name),
-                                      "measure": cook_book[dish].get(measure),
-                                      "quantity": cook_book[dish].get(quantity) * (person_count)})
-                print(list_products)
+        if dish in cook_book:
+            for ingredient in cook_book[dish]:
+                ingredient_name = ingredient['ingredient']
+                quantity = ingredient['quantity']*person_count
+                measure = ingredient['measure']
+                if ingredient_name in list_products:
+                    list_products[ingredient_name]['quantity'] += quantity
+                else:
+                    list_products[ingredient_name] = {'measure': measure, 'quantity': quantity}
         else:
-            print(f'Блюда "{dishes}" нет в книге рецептов')
+            print(f'Блюда {dish} нет в книге рецептов')
+            return list_products
 
 
-get_shop_list_by_dishes(['Запеченный картофель', 'Фахитос', 'Омлет'], 4)
+shop = get_shop_list_by_dishes(['Запеченный картофель', 'Фахитос', 'Омлет'], 4)
+print(shop)
 
 
 
--- test.py
+++ test.py
@@ -1,3 +1,36 @@
+with open("cook.txt") as file:
+    cook_book = {}
+    for line in file:
+        name_dish = line.strip()
+        number_ingredients = int(file.readline())
+        ingredients = []
+        for number in range(number_ingredients):
+            ingredient = file.readline().strip()
+            ingredient_name, quantity, measure = ingredient.split(' | ')
+            ingredients.append({'ingredient': ingredient_name, 'quantity': int(quantity), 'measure': measure})
+        file.readline()
+        cook_book[name_dish] = ingredients
+print(cook_book)
 
+def get_shop_list_by_dishes(dishes, person_count):
+    list_products = {}
+    for dish in dishes:
+        if dish in cook_book:
+            for ingredient in cook_book[dish]:
+                ingredient_name = ingredient['ingredient']
+                quantity = ingredient['quantity'] * person_count
+                measure = ingredient['measure']
+
+                if ingredient_name in list_products:
+                    list_products[ingredient_name]['quantity'] += quantity
+                else:
+                    list_products[ingredient_name] = {'measure': measure, 'quantity': quantity}
+        else:
+            print(f'Блюда "{dish}" нет в книге рецептов')
+
+    return list_products
+
+shop_list = get_shop_list_by_dishes(['Запеченный картофель', 'Фахитос'], 4)
+print(shop_list)
 
 
